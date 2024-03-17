3e264a74bd4a5e56d96c16b0916fb0a2d6e03565
blakeminix <blakeminix2@gmail.com>
2024-03-17 10:08:52 -0400

    Portfolio Start

--- src/App.js
+++ src/App.js
@@ -5,10 +5,34 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div className='top'>
         <img src={blake} className="App-logo" alt="logo" />
-        <p>
+        <h2>
           Blake Minix
+        </h2>
+        </div>
+        <div className='section'>
+        <p className='header-text'>My Links</p>
+        <div className='link-list'>
+          <a href='https://github.com/blakeminix' className='App-link' target='_blank' rel="noopener noreferrer">GitHub</a>
+          <a href='https://www.linkedin.com/in/blakeminix/' className='App-link' target="_blank" rel="noopener noreferrer">LinkedIn</a>
+        </div>
+        </div>
+        <div className='section'>
+        <p className='header-text'>About Me</p>
+      <p className='custom-text'>
+          3rd Year Computer Science Student
         </p>
+        <p className='custom-text'>
+          Georgia Institute of Technology
+        </p>
+        <p className='custom-text'>
+          Threads: Systems & Architecture and Information Internetworks
+        </p>
+        <p className='custom-text'>
+          Expected Graduation Date: Fall 2024
+        </p>
+        </div>
       </header>
     </div>
   );
